25a9dc357b3e7640ae52afcf635f948349fc46bc
maggieeagle <oryol.margo@gmail.com>
2023-09-24 19:43:03 +0300

    add more links to routes

--- static/scripts/spa.js
+++ static/scripts/spa.js
@@ -1,5 +1,5 @@
 document.addEventListener('click', e => {
-    if (["home-nav", "create-post-nav"].includes(e.target.id)) {
+    if (["home-nav", "create-post-nav", "profile-nav", "sign-out-nav", "register-link"].includes(e.target.id)) {
         route(e)
     }
     e.preventDefault();
@@ -13,9 +13,10 @@ const route = (e) => {
 const routers = {
     '/home': 'home.html',
     '/create-post': 'create-post.html',
-    '/profile': 'profile.html',
+    '/view-profile': 'profile.html',
+    '/sign-out': 'sign-in.html',
     '/sing-in': '',
-    '/sign-up': ''
+    '/sign-up': 'sign-up.html'
 }
 
 const handleLocation = async () => {
@@ -23,11 +24,22 @@ const handleLocation = async () => {
     const html = await fetch('./static/templates/' + routers[path]).then((data) => data.text()).catch(error => {
         console.error('Error:', error);
     });
-    // if (path === "/sign-in" || path === 'sign-up') {
-
-    // } else {
-        document.querySelector('#content-container').innerHTML = html;
-    // }
+    console.log(html)
+    if (path === "/sign-out") {
+        document.body.innerHTML = html;
+    } else {
+        if (path === "/sign-up") {
+            const header = await fetch('./static/templates/header.html').then((data) => data.text());
+            document.body.innerHTML = header + html;
+        } else {
+            const header = await fetch('./static/templates/header.html').then((data) => data.text());
+            const sidepanel = await fetch('./static/templates/sidepanel.html').then((data) => data.text());
+            document.body.innerHTML = header + sidepanel;
+            document.querySelector('#content-container').innerHTML = html;
+        }
+    }
 }
 
+window.onpopstate = handleLocation;
 window.route = route;
+handleLocation();
