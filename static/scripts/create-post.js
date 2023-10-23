73c9bffcf9389c1dbc9165abb717f9a3b8e0f891
Antosha7 <litvintsevanton@gmail.com>
2023-10-23 20:40:51 +0300

    add create post form validation

--- static/scripts/create-post.js
+++ static/scripts/create-post.js
@@ -21,25 +21,29 @@ function changeDropdownText(item) {
 document.addEventListener('click', function (e) {
     if (location.pathname === "/create-post") {
         if (e.target.matches('#post-submit-btn')) {
-            e.preventDefault();
-            let x = document.querySelector('form.create-post').elements;
+            if (document.querySelector('form.create-post')['titleCategory'].innerHTML == "Select Category") {
+                alert("Please select category")
+            }else if (document.querySelector('form.create-post').checkValidity()) {
+                e.preventDefault();
+                let x = document.querySelector('form.create-post').elements;
 
-            let formData = new FormData();
-            formData.append('thread', document.querySelector('form.create-post')['titleCategory'].innerHTML);
-            formData.append('title', document.querySelector('form.create-post #create-post-title').value);
-            formData.append('image', document.querySelector('form.create-post #create-post-img').files[0]);
-            formData.append('image-name', document.querySelector('form.create-post #create-post-img').value.split(/(\\|\/)/g).pop());
-            formData.append('content', document.querySelector('form.create-post #create-post-content').value);
+                let formData = new FormData();
+                formData.append('thread', document.querySelector('form.create-post')['titleCategory'].innerHTML);
+                formData.append('title', document.querySelector('form.create-post #create-post-title').value);
+                formData.append('image', document.querySelector('form.create-post #create-post-img').files[0]);
+                formData.append('image-name', document.querySelector('form.create-post #create-post-img').value.split(/(\\|\/)/g).pop());
+                formData.append('content', document.querySelector('form.create-post #create-post-content').value);
 
-            fetch('http://127.0.0.1:8080/api/add-post', {
-                method: 'POST',
-                headers: {
-                    'credentials': 'include',
-                },
-                body: formData
-            }).then(response => {
-                if (response.ok) { window.location.href = '/home' } else { /*display error*/ }
-            })
+                fetch('http://127.0.0.1:8080/api/add-post', {
+                    method: 'POST',
+                    headers: {
+                        'credentials': 'include',
+                    },
+                    body: formData
+                }).then(response => {
+                    if (response.ok) { window.location.href = '/home' } else { /*display error*/ }
+                })
+            }
         }
     }
 })
